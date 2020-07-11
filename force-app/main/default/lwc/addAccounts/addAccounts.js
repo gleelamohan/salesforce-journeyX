8528328726a9d7e7582a578efef06b62964f608f
unknown <leelamohan.g@gmail.com>
2020-07-11 21:14:56 +0530

    add account using record edit form

--- force-app/main/default/lwc/addAccounts/addAccounts.js
+++ force-app/main/default/lwc/addAccounts/addAccounts.js
@@ -1,3 +1,31 @@
 import { LightningElement } from 'lwc';
 
-export default class AddAccounts extends LightningElement {}
+export default class AddAccounts extends LightningElement {
+
+    accountId = '';
+    handleSuccess(event) {
+        this.accountId = event.detail.id;
+        console.log(this.accountId);
+    }
+
+    get acceptedFormats() {
+        return ['.png', '.jpg', '.jpeg'];
+    }
+
+    get isUpload() {
+        return this.accountId != '' ? true : false;
+    }
+
+    saveClick(event) {
+        //const myfield = event.detail.fields;
+        //myfield.field4 = "Hello";
+        this.template.querySelector('lightning-record-edit-form').submit();
+    }
+
+    handleUploadFinished(event) {
+        const uploadedFiles = event.detail.files;
+        console.log("No. of files uploaded : " + uploadedFiles.length);
+    }
+
+
+}
