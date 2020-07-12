1a7952d0d1c17e4752d8d39cf5b847306486db55
unknown <leelamohan.g@gmail.com>
2020-07-12 23:32:18 +0530

    Add Account changes

--- force-app/main/default/lwc/addAccounts/addAccounts.js
+++ force-app/main/default/lwc/addAccounts/addAccounts.js
@@ -1,11 +1,18 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
+import updateAttachment from '@salesforce/apex/XpAccountController.updateAccountAttachment';
 
 export default class AddAccounts extends LightningElement {
 
+    @api userId;
     accountId = '';
+    documentId = '';
     handleSuccess(event) {
         this.accountId = event.detail.id;
         console.log(this.accountId);
+
+        this.updateAttachmentId();
+
+
     }
 
     get acceptedFormats() {
@@ -16,6 +23,16 @@ export default class AddAccounts extends LightningElement {
         return this.accountId != '' ? true : false;
     }
 
+    updateAttachmentId() {
+        updateAttachment({ accountId: this.accountId, documentId: this.documentId })
+            .then(result => {
+                this.contacts = result;
+            })
+            .catch(error => {
+                this.error = error;
+            });
+    }
+
     saveClick(event) {
         //const myfield = event.detail.fields;
         //myfield.field4 = "Hello";
@@ -24,6 +41,7 @@ export default class AddAccounts extends LightningElement {
 
     handleUploadFinished(event) {
         const uploadedFiles = event.detail.files;
+        this.documentId = uploadedFiles[0].documentId;
         console.log("No. of files uploaded : " + uploadedFiles.length);
     }
 
--- force-app/main/default/lwc/createExperience/createExperience.js
+++ force-app/main/default/lwc/createExperience/createExperience.js
@@ -1,3 +1,27 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire, track } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
+import USER_ID from '@salesforce/user/Id';
+import NAME_FIELD from '@salesforce/schema/User.Name';
 
-export default class CreateExperience extends LightningElement {}
+export default class CreateExperience extends LightningElement {
+
+    @track error;
+    @track name;
+    @track id;
+    @wire(getRecord, {
+        recordId: USER_ID,
+        fields: [NAME_FIELD]
+    }) wireuser({
+        error,
+        data
+    }) {
+        if (error) {
+            this.error = error;
+        } else if (data) {
+            console.log(data);
+            this.name = data.fields.Name.value;
+            this.id = data.id;
+        }
+    }
+
+}
