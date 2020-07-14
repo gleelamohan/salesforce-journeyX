4d22add36edbe701fb2a86502430d0b8a772ae15
Surendranadh Nune <snune@salesforce.com>
2020-07-15 01:27:12 +0530

    Committing Add Contacts, Display Contacts

--- force-app/main/default/lwc/addContacts/addContacts.js
+++ force-app/main/default/lwc/addContacts/addContacts.js
@@ -1,28 +1,15 @@
 import { LightningElement, api } from "lwc";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
 export default class AddContacts extends LightningElement {
-  contactsForDisplay = [];
   @api accountId;
-  columns = [
-    { label: "Full Name", fieldName: "Name", type: "text", editable: true },
-    { label: "Email", fieldName: "Email__c", type: "email", editable: true },
-    {
-      label: "Contact Number",
-      fieldName: "Contact_Number__c",
-      type: "phone",
-      editable: true
-    },
-    { label: "Role", fieldName: "Role__c", type: "text", editable: true }
-  ];
+  loadContacts = false;
   handleSuccess(event) {
-    let conRecord = {};
-    let fields = event.detail.fields;
-    conRecord.Id = event.detail.id;
-    conRecord["Name"] = fields["Name"].value;
-    conRecord["Role__c"] = fields.Role__c.value;
-    conRecord["Email__c"] = fields.Email__c.value;
-    conRecord["Contact_Number__c"] = fields.Contact_Number__c.value;
-    this.contactsForDisplay = [...this.contactsForDisplay, conRecord];
+    if (!this.loadContacts) {
+      this.loadContacts = true;
+    } else {
+      this.template.querySelector("c-display-contacts").reloadContactData();
+    }
+
     this.showToast();
     this.resetForm();
   }
---
+++ force-app/main/default/lwc/displayContacts/displayContacts.js
@@ -0,0 +1,136 @@
+import { LightningElement, api, wire } from "lwc";
+import getContactList from "@salesforce/apex/XP_ContactController.fetchContacts";
+import { deleteRecord } from "lightning/uiRecordApi";
+import { refreshApex } from "@salesforce/apex";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+
+export default class DisplayContacts extends LightningElement {
+  actions = [
+    { label: "Edit", name: "edit" },
+    { label: "Delete", name: "delete" }
+  ];
+  columns = [
+    { label: "Full Name", fieldName: "Name", type: "text" },
+    { label: "Email", fieldName: "Email__c", type: "email" },
+    {
+      label: "Contact Number",
+      fieldName: "Contact_Number__c",
+      type: "phone"
+    },
+    { label: "Role", fieldName: "Role__c", type: "text" },
+    {
+      type: "action",
+      typeAttributes: { rowActions: this.actions }
+    }
+  ];
+  @api accountId = "a013h000008kzPjAAI";
+  @wire(getContactList, { accId: "$accountId" })
+  contact;
+
+  showConfirmDialog = false;
+  showConEditForm = false;
+  selrecId = "";
+
+  @api reloadContactData() {
+    return refreshApex(this.contact);
+  }
+  handleConfirmDialogNo() {
+    this.showConfirmDialog = false;
+    this.selrecId = "";
+  }
+  handleConfirmDialogYes(event) {
+    this.showConfirmDialog = false;
+    this.selrecId = event.target.dataset.recid;
+    this.deleteContact();
+  }
+  deleteContact() {
+    deleteRecord(this.selrecId)
+      .then(() => {
+        this.dispatchEvent(
+          new ShowToastEvent({
+            title: "Success",
+            message: "Contact deleted successfully.",
+            variant: "success"
+          })
+        );
+        return refreshApex(this.contact);
+      })
+      .catch((error) => {
+        this.dispatchEvent(
+          new ShowToastEvent({
+            title: "Error deleting record",
+            message: error.body.message,
+            variant: "error"
+          })
+        );
+      });
+  }
+  handleRowAction(event) {
+    const actionName = event.detail.action.name;
+    const row = event.detail.row;
+    switch (actionName) {
+      case "delete":
+        this.selrecId = row.Id;
+        this.showConfirmDialog = true;
+        break;
+      case "edit":
+        this.showConEditForm = true;
+        this.selrecId = row.Id;
+        break;
+    }
+  }
+  saveContact() {
+    this.template.querySelector("lightning-record-edit-form").submit();
+  }
+  handleSuccess(event) {
+    this.showToast();
+    this.selrecId = "";
+    this.showConEditForm = false;
+    return refreshApex(this.contact);
+  }
+  /*handleSave(event) {
+    const fields = {};
+    console.log("id***" + JSON.stringify(event.detail));
+    fields[ID_FIELD.fieldApiName] = event.detail.id;
+    fields[NAME_FIELD.fieldApiName] = event.detail.fields.Name.value;
+    fields[EMAIL_FIELD.fieldApiName] = event.detail.fields.Email__c.value;
+    fields[CONTACT_NUMBER_FIELD.fieldApiName] =
+      event.detail.draftValues[0].Contact_Number__c;
+    fields[ROLE_FIELD.fieldApiName] = event.detail.draftValues[0].Role__c;
+
+    const recordInput = { fields };
+
+    updateRecord(recordInput)
+      .then(() => {
+        this.dispatchEvent(
+          new ShowToastEvent({
+            title: "Success",
+            message: "Contact updated",
+            variant: "success"
+          })
+        );
+        // Clear all draft values
+        this.draftValues = [];
+
+        // Display fresh data in the datatable
+        return refreshApex(this.contact);
+      })
+      .catch((error) => {
+        this.dispatchEvent(
+          new ShowToastEvent({
+            title: "Error creating record",
+            message: error.body.message,
+            variant: "error"
+          })
+        );
+      });
+  }*/
+  showToast() {
+    const event = new ShowToastEvent({
+      title: "Success!",
+      variant: "success",
+      message: "Contact Saved successfully."
+    });
+    this.dispatchEvent(event);
+  }
+}
