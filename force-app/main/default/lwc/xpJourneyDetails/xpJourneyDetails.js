f75cc3e726478c7a356a0d7d5eaf7b44cde82b9c
Leela Mohan <lmohan@salesforce.com>
2020-07-28 16:52:18 +0530

    Journey Stage

--- force-app/main/default/lwc/xpJourneyDetails/xpJourneyDetails.js
+++ force-app/main/default/lwc/xpJourneyDetails/xpJourneyDetails.js
@@ -1,11 +1,51 @@
-import { LightningElement, api } from "lwc";
+import { LightningElement, api, track } from "lwc";
+import fetchJourneys from "@salesforce/apex/XP_JourneyController.fetchJourneys";
 
 export default class XpJourneyDetails extends LightningElement {
   @api accountId;
   @api xperienceId;
   @api executiveId;
+  @track journeyInfo;
+  journeyValue = "Select Experience Name";
+  description;
+  key;
+  journeyImage;
+  @track dropdown = [];
+
+  connectedCallback() {
+    this.getJourneys();
+  }
+
   handleGoBack() {
     console.log("in goback");
     this.dispatchEvent(new CustomEvent("backtoexecutive"));
+  }
+
+  getJourneys() {
+    fetchJourneys().then((result) => {
+      console.log(result);
+      this.journeyInfo = result;
+
+      this.dropdown = this.journeyInfo.reduce((acc, item) => {
+        acc.push({ label: item.journey.Name, value: item.journey.Name });
+        return acc;
+      }, []);
+      this.dropdown.splice(0, 0, {
+        label: "Select Experience Name",
+        value: "Select Experience Name"
+      });
+    });
+  }
+
+  handleChange(event) {
+    this.journeyValue = event.detail.value;
+
+    let selectedJourney = this.journeyInfo.filter((item) => {
+      return (item.journey.Name = this.journeyValue);
+    });
+
+    this.description = selectedJourney[0].journey.Description__c;
+    this.key = selectedJourney[0].journey.Journey_Id__c;
+    this.journeyImage = '/sfc/servlet.shepherd/version/download/'+selectedJourney[0].cdLink.ContentDocument.LatestPublishedVersionId;
   }
 }
